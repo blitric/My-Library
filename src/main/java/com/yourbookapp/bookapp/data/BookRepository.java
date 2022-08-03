e349a8580d8f945237b2da4c5d7bad598eb52b43
Alma <alma.blitric@gmail.com>
2022-08-03 16:40:23 -0500

    Started coding Book Model & Controller

--- src/main/java/com/yourbookapp/bookapp/data/BookRepository.java
+++ src/main/java/com/yourbookapp/bookapp/data/BookRepository.java
@@ -1,2 +1,9 @@
-package com.yourbookapp.bookapp.data;public interface BookRepository {
+package com.yourbookapp.bookapp.data;
+
+import com.yourbookapp.bookapp.models.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Integer> {
 }
--- src/main/java/com/yourbookapp/bookapp/models/Book.java
+++ src/main/java/com/yourbookapp/bookapp/models/Book.java
@@ -1,2 +1,58 @@
-package com.yourbookapp.bookapp.models;public class Book {
+package com.yourbookapp.bookapp.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book extends AbstractEntity {
+
+    private String name;
+    private String isbn;
+    private String published;
+    private int averageRating;
+
+    public Book(String name, String isbn, String published, int averageRating) {
+        this.name = name;
+        this.isbn = isbn;
+        this.published = published;
+        this.averageRating = averageRating;
+    }
+
+    public Book() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public int getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(int averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
